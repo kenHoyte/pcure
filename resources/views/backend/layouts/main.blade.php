eb2f447173056a468b75cf0ae7a300d4dab4e7aa
Ransford Boakye <Ransford.Boakye@prudentialbank.local>
2024-06-27 09:41:56 +0000

    Updates

--- resources/views/backend/layouts/main.blade.php
+++ resources/views/backend/layouts/main.blade.php
@@ -3,7 +3,7 @@
 
     <head>
         <meta charset="utf-8" />
-        <title>Collection</title>
+        <title>Procurement</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
         <meta content="DTI" name="author" />
